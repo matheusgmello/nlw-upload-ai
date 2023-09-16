a93988f8b41dc02431e0292a1c2582a82613c315
Matheus Gabriel <matheusgmello1@gmail.com>
2023-09-16 13:02:10 -0300

    Feat: add AI dependency

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,14 +1,14 @@
-import { fastify } from 'fastify'
-import { fastifyCors} from '@fastify/cors'
-import { getAllPromptsRoute } from './routes/get-all-prompts';
-import { uploadVideoRoute } from './routes/upload-video';
-import { createTranscriptionRoute } from './routes/create-transcription'
-import { generateAiCompletionRoute} from './routes/generate-ai-completion'
+import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify()
 
-app.register(fastifyCors,{
-    origin: '*'
+app.register(fastifyCors, {
+  origin: '*'
 })
 
 app.register(getAllPromptsRoute)
@@ -17,7 +17,7 @@ app.register(createTranscriptionRoute)
 app.register(generateAiCompletionRoute)
 
 app.listen({
-    port:3333,
-}).then(() =>{
-    console.log('HTTP Server is Running!');
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server Running!')
 })
